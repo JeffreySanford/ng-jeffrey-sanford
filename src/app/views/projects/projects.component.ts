1e6d7b2b6e0e371521d087525986da87dd51c972
Jeffrey Sanford <jeffreysanford@gmail.com>
2017-11-24 16:11:19 -0700

    setting the screen positions and tempaltes

--- src/app/views/projects/projects.component.ts
+++ src/app/views/projects/projects.component.ts
@@ -11,6 +11,29 @@ export class ProjectsComponent implements OnInit {
   private apiUrl = 'http://localhost:3000';
   data: any = {};
 
+  projects: any = {
+    featured: [
+      {
+        id: 0,
+        title: "Imagine Create Thrive",
+        location: "http://imaginecreatethrive.com",
+        description: "This is a small botique agency that can create mockups, designs, and web applciations."
+      },
+      {
+        id: 1,
+        title: "Gracious Girls",
+        location: "http://imaginecreatethrive.com:4200",
+        description: "This is a cute organization and cleaning oritented business."
+      },
+      {
+        id: 2,
+        title: "BS Native Girl",
+        location: "http://imaginecreatethrive.com:4201",
+        description: "Landing site presenting Native American are from an artist in the Pacific Northwest."
+      }
+    ]
+  }
+
   constructor(private http: Http) {
     console.log('User accessed API.');
     this.getContacts();
