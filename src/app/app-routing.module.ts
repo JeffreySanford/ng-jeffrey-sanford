e5eff4842e769e39c0d5e3d1d863eb5cbf50f35a
Jeffrey Sanford <jeffrey@imaginecreatethrive.com>
2021-10-15 14:48:57 -0600

    add landing skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing', component: LandingComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
