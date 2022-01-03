85064a00967b0acbb3c562fb7f92fd3b4df0c220
Jeffrey <jeffrey@imaginecreatethrive.com>
2022-01-03 15:12:40 -0600

    Upgrade to 13

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './app-landing/landing.component';
+import { ArchitectureComponent } from './architecture/architecture.component';
+import { DesignComponent } from './design/design.component';
+import { DevelopmentComponent } from './development/development.component';
 import { TableComponent } from './samples/table/table.component';
 
 const routes: Routes = [
   { path: 'landing', component: LandingComponent },
-  { path: 'samples/sample-table', component: TableComponent }
+  { path: 'samples/sample-table', component: TableComponent },
+  { path: 'design', component: DesignComponent },
+  { path: 'developemnt', component: DevelopmentComponent },
+  { path: 'architecture', component: ArchitectureComponent }
 ];
 
 @NgModule({
