268bab1cd318e2ecd0d4e185b6a9a3839ebfba4d
Jeffrey <jeffrey@imaginecreatethrive.com>
2022-01-23 19:28:41 -0600

    adjust route for space video

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { ArchitectureComponent } from './samples/architecture/architecture.compo
 import { DesignComponent } from './samples/design/design.component';
 import { DevelopmentComponent } from './samples/development/development.component';
 import { TableComponent } from './samples/design/table/table.component';
+import { SpaceVideoComponent } from './samples/design/space-video/space-video.component';
 
 const routes: Routes = [
   { path: 'landing', component: LandingComponent },
   { path: 'samples/sample-table', component: TableComponent },
+  { path: 'samples/space-video', component: SpaceVideoComponent },
   { path: 'design-dashboard', component: DesignComponent },
   { path: 'development-dashboard', component: DevelopmentComponent },
   { path: 'architecture-dashboard', component: ArchitectureComponent }
