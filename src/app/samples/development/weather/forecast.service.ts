46516e313006cf3147059084c968b3d5f6cf6699
Jeffrey <jeffrey@imaginecreatethrive.com>
2022-03-22 10:55:24 -0500

    landing should trigger an all black screen as opposed to the other pages.

---
+++ src/app/samples/development/weather/forecast.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ForecastDetails } from './weather-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ForecastService {
+  forecastData!: ForecastDetails;
+  showCurrent: boolean = true;
+  showForecast: boolean = true;
+  zipcode!: string;
+  forecastDetails!: Array<ForecastDetails>
+
+  constructor(private http: HttpClient) { }
+
+  LoadForecastWeather(zip: any): Observable<any> {
+    return this.http.get("https://api.openweathermap.org/data/2.5/forecast?zip="+zip+",us&APPID=dabc2b57d81c4493c08ab63bb4d9e326&units=imperial" );
+  }
+
+  LoadCurrentWeather(zip: any): Observable<any> {
+    return this.http.get("https://api.openweathermap.org/data/2.5/weather?zip="+zip+",us&APPID=dabc2b57d81c4493c08ab63bb4d9e326&units=imperial" );
+  }
+}
